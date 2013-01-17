b78f34c5d879e234a8ef541d188fcfac6a87ed63
Gustavo Guichard <gustavoguichard@gmail.com>
2013-01-17 10:42:25 -0200

    Still trying to connect to mongodb on heroku

--- db/connect.coffee
+++ db/connect.coffee
@@ -1,4 +1,15 @@
 mongoose = require 'mongoose'
-mongoose.connect process.env.MONGOLAB_URI || 'mongodb://localhost/helloExpress'
+
+uristring = process.env.MONGODB_URI || process.env.MONGOLAB_URI || 'mongodb://localhost/helloExpress'
+
+# Ensure safe writes
+mongoOptions = { db: { safe: true }}
+
+# Connect
+mongoose.connect uristring, mongoOptions, (err, res)->
+  if err
+    console.log "ERROR connecting to: #{uristring}. #{err}"
+  else
+    console.log "Succeeded connected to: #{uristring}"
 
 module.exports = mongoose
--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,21 @@
-var mongoose;
+var mongoOptions, mongoose, uristring;
 
 mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost/helloExpress");
+uristring = process.env.MONGODB_URI || process.env.MONGOLAB_URI || 'mongodb://localhost/helloExpress';
+
+mongoOptions = {
+  db: {
+    safe: true
+  }
+};
+
+mongoose.connect(uristring, mongoOptions, function(err, res) {
+  if (err) {
+    return console.log("ERROR connecting to: " + uristring + ". " + err);
+  } else {
+    return console.log("Succeeded connected to: " + uristring);
+  }
+});
 
 module.exports = mongoose;
